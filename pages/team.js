f183fcf2d367dca72e53a1f1975dd2cfe61d487b
ShahbazSherwani <m.shahbazsherwani@gmail.com>
2022-09-04 17:20:49 +0400

    OurStory
    Please do not merge it to main.

---
+++ pages/team.js
@@ -0,0 +1,200 @@
+import Image from "next/image";
+import aprilImg from "/assets/04_Arqon_Our Story_April.jpg";
+import mannyImg from "/assets/04_Arqon_Our Story_Manny.jpg";
+import chinoImg from "/assets/04_Arqon_Our Story_Chino.jpg";
+import paoloImg from "/assets/04_Arqon_Our Story_Paolo.jpg";
+import joshuaImg from "/assets/04_Arqon_Our Story_Joshua.jpg";
+import koebeeImg from "/assets/04_Arqon_Our Story_Koebee.jpg";
+
+export default function team() {
+  return (
+    <div className="story">
+      <div class="container pt-5 pb-5">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="vc_column-inner">
+              <div class="team-member dark">
+                <div class="content-box">
+                  <div class="image-container">
+                    <Image src={aprilImg} alt="Ms. April" />
+                    <div class="side-overlay"></div>
+                    <div class="vertical-number-box">
+                      <span class="vertical-number">Managing Director</span>
+                    </div>
+                    <div class="member-details">
+                      <hr></hr>
+                      <span class="member-info">
+                        Thanks to his extensive international experience, Stanko
+                        has gathered a community of creativity and innovation
+                        experts, always providing insights and advice on getting
+                        better ideas, how to implement them and provide greater
+                        value in complete interior or exterior design and decor.
+                      </span>
+                      <hr></hr>
+                    </div>
+                  </div>
+                  <div class="content-box-info">
+                    <h4>Arch. April Pulido</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="vc_column-inner">
+              <div class="team-member dark">
+                <div class="content-box">
+                  <div class="image-container">
+                    <Image src={mannyImg} alt="Manny Pulido" />
+                    <div class="side-overlay"></div>
+                    <div class="vertical-number-box">
+                      <span class="vertical-number">
+                        Director | Designer | BIM Manager
+                      </span>
+                    </div>
+                    <div class="member-details">
+                      <hr></hr>
+                      <span class="member-info">
+                        Thanks to his extensive international experience, Stanko
+                        has gathered a community of creativity and innovation
+                        experts, always providing insights and advice on getting
+                        better ideas, how to implement them and provide greater
+                        value in complete interior or exterior design and decor.
+                      </span>
+                      <hr></hr>
+                    </div>
+                  </div>
+                  <div class="content-box-info">
+                    <h4>Arch. Manny Pulido</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="vc_column-inner">
+              <div class="team-member dark">
+                <div class="content-box">
+                  <div class="image-container">
+                    <Image src={chinoImg} alt="Chino Buitizon" />
+                    <div class="side-overlay"></div>
+                    <div class="vertical-number-box">
+                      <span class="vertical-number">
+                        Director | Designer | Project Manager
+                      </span>
+                    </div>
+                    <div class="member-details">
+                      <hr></hr>
+                      <span class="member-info">
+                        Thanks to his extensive international experience, Stanko
+                        has gathered a community of creativity and innovation
+                        experts, always providing insights and advice on getting
+                        better ideas, how to implement them and provide greater
+                        value in complete interior or exterior design and decor.
+                      </span>
+                      <hr></hr>
+                    </div>
+                  </div>
+                  <div class="content-box-info">
+                    <h4>Arch. Chino Buitizon</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container pt-5 pb-5">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="vc_column-inner">
+              <div class="team-member dark">
+                <div class="content-box">
+                  <div class="image-container">
+                    <Image src={paoloImg} alt="Paolo Manlapaz" />
+                    <div class="side-overlay"></div>
+                    <div class="vertical-number-box">
+                      <span class="vertical-number">Site Engineer</span>
+                    </div>
+                    <div class="member-details">
+                      <hr></hr>
+                      <span class="member-info">
+                        Thanks to his extensive international experience, Stanko
+                        has gathered a community of creativity and innovation
+                        experts, always providing insights and advice on getting
+                        better ideas, how to implement them and provide greater
+                        value in complete interior or exterior design and decor.
+                      </span>
+                      <hr></hr>
+                    </div>
+                  </div>
+                  <div class="content-box-info">
+                    <h4>Engr. Paolo Manlapaz</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="vc_column-inner">
+              <div class="team-member dark">
+                <div class="content-box">
+                  <div class="image-container">
+                    <Image src={joshuaImg} alt="Joshua Aquino" />
+                    <div class="side-overlay"></div>
+                    <div class="vertical-number-box">
+                      <span class="vertical-number">Junior Architect</span>
+                    </div>
+                    <div class="member-details">
+                      <hr></hr>
+                      <span class="member-info">
+                        Thanks to his extensive international experience, Stanko
+                        has gathered a community of creativity and innovation
+                        experts, always providing insights and advice on getting
+                        better ideas, how to implement them and provide greater
+                        value in complete interior or exterior design and decor.
+                      </span>
+                      <hr></hr>
+                    </div>
+                  </div>
+                  <div class="content-box-info">
+                    <h4>Arch. Joshua Aquino</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="vc_column-inner">
+              <div class="team-member dark">
+                <div class="content-box">
+                  <div class="image-container">
+                    <Image src={koebeeImg} alt="Koebee Cabigao" />
+                    <div class="side-overlay"></div>
+                    <div class="vertical-number-box">
+                      <span class="vertical-number">Admin and Accounting</span>
+                    </div>
+                    <div class="member-details">
+                      <hr></hr>
+                      <span class="member-info">
+                        Thanks to his extensive international experience, Stanko
+                        has gathered a community of creativity and innovation
+                        experts, always providing insights and advice on getting
+                        better ideas, how to implement them and provide greater
+                        value in complete interior or exterior design and decor.
+                      </span>
+                      <hr></hr>
+                    </div>
+                  </div>
+                  <div class="content-box-info">
+                    <h4>Koebee Cabigao</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
