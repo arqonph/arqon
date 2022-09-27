c68f64656f2c292d19d18208cc2b1957bfa5145f
ShahbazSherwani <m.shahbazsherwani@gmail.com>
2022-09-27 14:45:41 +0400

    All changes Complete
    Ready to Merge to Main

---
+++ components/landing/Team.js
@@ -0,0 +1,242 @@
+import Image from "next/image";
+import aprilImg from "/assets/04_Arqon_Our Story_April.jpg";
+import mannyImg from "/assets/04_Arqon_Our Story_Manny.jpg";
+import chinoImg from "/assets/04_Arqon_Our Story_Chino.jpg";
+import paoloImg from "/assets/04_Arqon_Our Story_Paolo.jpg";
+import joshuaImg from "/assets/04_Arqon_Our Story_Joshua.jpg";
+import koebeeImg from "/assets/04_Arqon_Our Story_Koebee.jpg";
+
+export default function team() {
+  return (
+    <div className="story">
+      {/* <div className="container pt-5 pb-5">
+        <div className="row">
+          <div className="container col-xxl-4 col-lg-4 col-md-6 mb-5 aprilMD">
+            <div className="aprilImg position-relative">
+              <div className="contentBox">
+                <span className="">Arch. April Pulido</span>
+              </div>
+              <div className="descriptionOverlay">
+                <div className="info">
+                  <span>
+                    April’s previous works as Senior Architect at GF and
+                    Partners Architects for 8 years greatly contributed to the
+                    firm’s prestigious recognitions by BCI Asia as one of the
+                    top 10 architectural firms in the country. Her skill and
+                    experience have helped her take on some of the most
+                    celebrated projects of Ayala Land Premiere including upscale
+                    residential condominiums, high-end commercial structures and
+                    triple A offices.
+                    <br></br>
+                    <br></br>
+                    <nbspan>
+                      A former collegiate volleyball player at the University of
+                      Santo Tomas and self professed travel lover, this
+                      multi-hyphenate also includes professional wedding
+                      photographer as one of her many titles. April will be
+                      taking her Master of Business Administration (MBA) in the
+                      Ateneo Graduate School of Business under the Ateneo-Regis
+                      program this academic year.
+                    </nbspan>
+                    <br className="pt-5"></br>
+                    <a className="linkedin" href="https://www.linkedin.com/">
+                      LinkedIn:
+                    </a>
+                  </span>
+                </div>
+                <div className="contentBoxOverlay">
+                  <span className="">Arch. April Pulido</span>
+                </div>
+              </div>
+              <ul className="vertical-box">
+                <li className="verticl-text">
+                  <span className="designation">Managing Director</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="container col-xxl-4 col-lg-4 col-md-6 mb-5 mannyBIM">
+            <div className="mannyImg position-relative">
+              <div className="contentBox">
+                <span className="">Arch. Manny Pulido</span>
+              </div>
+              <div className="descriptionOverlay">
+                <div className="info">
+                  <span>
+                    Manny’s journey to being a founding partner of ARQON began
+                    way before his College of Architecture days at University of
+                    Santo Tomas, where he graduated cum laude. His passion for
+                    freehand drawing and rendering built the foundations of his
+                    career and his stint as Asst. Designer at RR Payumo and
+                    Partners and developed his natural eye for thoughtful
+                    functional design. As Studio BIM Manager at Aidea
+                    Philippines, he was part of the team that bagged the
+                    prestigious international award for Sustainability,
+                    Buildability and Constructability of the BIM Buzz in
+                    Singapore demonstrating how Building Information Modeling
+                    (BIM) can be used to its full potential.
+                    <br></br>
+                    <br></br>
+                    <nbspan>
+                      It was only a matter of time until Manny found himself
+                      training his own pool of talented architects as BIM
+                      Manager at GF and Partners Architects, one of the
+                      Philippines’ most prominent architectural firms. LinkedIn:
+                    </nbspan>
+                    <br className="pt-5"></br>
+                    <a className="linkedin" href="https://www.linkedin.com/">
+                      LinkedIn:
+                    </a>
+                  </span>
+                </div>
+                <div className="contentBoxOverlay">
+                  <span className="">Arch. Manny Pulido</span>
+                </div>
+              </div>
+              <ul className="vertical-box">
+                <li className="verticl-text">
+                  <span className="designation">
+                    Director | Designer | BIM Manager
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="container col-xxl-4 col-lg-4 col-md-6 mb-5 chinoPM">
+            <div className="chinoImg position-relative">
+              <div className="contentBox">
+                <span className="">Arch. Chino Buitizon</span>
+              </div>
+              <div className="descriptionOverlay">
+                <div className="info">
+                  <span>
+                    Chino’s scholastic foundation in Architecture already had
+                    him garnering recognition for his talents from his
+                    professors and peers at the University of Santo Tomas. His
+                    college thesis, which was given an outstanding distinction
+                    in his batch, was handpicked as the school’s official entry
+                    to the Archiprix 2007 Hunter Douglas awards held in
+                    Shanghai, China.
+                    <br></br>
+                    <br></br>
+                    <nbspan>
+                      His background with GF and Partners Architects in large
+                      scale residential and commercial projects include One
+                      Serenda, Bonifacio High Street, Marquee Mall and Abreeza
+                      Mall in Davao. He went on to pursue his professional
+                      practice where he designed and built residences in private
+                      subdivisions, as well as commercial developments and
+                      institutional centers.
+                    </nbspan>
+                    <br className="pt-5"></br>
+                    <a className="linkedin" href="https://www.linkedin.com/">
+                      LinkedIn:
+                    </a>
+                  </span>
+                </div>
+                <div className="contentBoxOverlay">
+                  <span className="">Arch. Chino Buitizon</span>
+                </div>
+              </div>
+              <ul className="vertical-box">
+                <li className="verticl-text">
+                  <span className="designation">
+                    Director | Designer | Project Manager
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <div className="container pb-5">
+        <div className="row">
+          <div className="container col-xxl-4 col-lg-4 col-md-6 mb-5 paoEng">
+            <div className="paoloImg position-relative">
+              <div className="contentBox">
+                <span className="">Engr. Paolo Manlapaz</span>
+              </div>
+              <div className="descriptionOverlay">
+                <div className="info">
+                  <span>
+                    <br></br>
+                    <br></br>
+                    <nbspan></nbspan>
+                    <br className="pt-5"></br>
+                    <a className="linkedin" href="https://www.linkedin.com/">
+                      LinkedIn:
+                    </a>
+                  </span>
+                </div>
+                <div className="contentBoxOverlay">
+                  <span className="">Engr. Paolo Manlapaz</span>
+                </div>
+              </div>
+              <ul className="vertical-box">
+                <li className="verticl-text">
+                  <span className="designation">Site Engineer</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="container col-xxl-4 col-lg-4 col-md-6 mb-5 joshArc">
+            <div className="joshImg position-relative">
+              <div className="contentBox">
+                <span className="">Arch. Joshua Aquino</span>
+              </div>
+              <div className="descriptionOverlay">
+                <div className="info">
+                  <span>
+                    <br></br>
+                    <br></br>
+                    <nbspan></nbspan>
+                    <br className="pt-5"></br>
+                    <a className="linkedin" href="https://www.linkedin.com/">
+                      LinkedIn:
+                    </a>
+                  </span>
+                </div>
+                <div className="contentBoxOverlay">
+                  <span className="">Arch. Joshua Aquino</span>
+                </div>
+              </div>
+              <ul className="vertical-box">
+                <li className="verticl-text">
+                  <span className="designation">Junior Architect</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="container col-xxl-4 col-lg-4 col-md-6 mb-5 koeAc">
+            <div className="koebeeImg position-relative">
+              <div className="contentBox">
+                <span className="">Koebee Cabigao</span>
+              </div>
+              <div className="descriptionOverlay">
+                <div className="info">
+                  <span>
+                    <br></br>
+                    <br></br>
+                    <nbspan></nbspan>
+                    <br className="pt-5"></br>
+                    <a className="linkedin" href="https://www.linkedin.com/">
+                      LinkedIn:
+                    </a>
+                  </span>
+                </div>
+                <div className="contentBoxOverlay">
+                  <span className="">Koebee Cabigao</span>
+                </div>
+              </div>
+              <ul className="vertical-box">
+                <li className="verticl-text">
+                  <span className="designation">Accounting Dept.</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/OurStory.js
@@ -0,0 +1,112 @@
+import Image from "next/image";
+import Head from "next/head";
+import Navbar from "../components/landing/Navigation-bar";
+import Footer from "../components/landing/Footer";
+import arqonTopImg from "/assets/01_Arqon_top_image.jpg";
+import arqonStoryImage from "../assets/Arqon_Our Story Group.jpg";
+import Team from "../components/landing/Team.js";
+import Heads from "../components/landing/Heads.js";
+
+export default function story() {
+  return (
+    <div className="story">
+      <Head>
+        <title>ARQON</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Website for ARQON" />
+        <link rel="icon" href="/Arqon-Logos-A-White.jpg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Navbar />
+      <div className="cover-container d-flex flex-column position-relative display-box">
+        <Image src={arqonTopImg} />
+        <div className="card-img-overlay">
+          <h1 className="card-title hero-text">Our Story</h1>
+        </div>
+      </div>
+      <div className="container section-1 text-center pt-5 pb-5 ">
+        <div className="row">
+          <div className="col">
+            <h1>
+              <span className="text-black">
+                It all started with our passion for
+              </span>
+              &nbsp;<span className="text-turquoise">TRAVEL</span>&nbsp;
+              <span className="text-black">and</span>&nbsp;
+              <span className="text-turquoise">DESIGN.</span>
+            </h1>
+            <hr className="hr-story"></hr>
+            <p>
+              Somewhere between touring the centuries-old building in Quebec,
+              and driving through the plains of Arizona and steel sky scrapers
+              of New York City, three architects on the trip of their lifetime
+              found themselves enthralled by the structural marvels and
+              innovative structures of the modern world.
+              <br></br>
+              <br></br>ARQON, proudly Filipino company with a global perspective
+              on design and build, was established not long after.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="cover-container d-flex flex-column position-relative display-box">
+        <Image src={arqonStoryImage} />
+      </div>
+      <div className="cover-container section-2">
+        <div className="container text-center pt-5 pb-5 ">
+          <div className="row">
+            <div className="col">
+              <h1 className="story-heading"> This is our story</h1>
+              <p>
+                <span className="text-story">
+                  Real frienships and honest relationships build the strong
+                  foundation of our company. <br></br> We’re motivated to be
+                </span>
+                &nbsp;
+                <span className="text-white">rock stars</span>
+                &nbsp;
+                <span className="text-story">of the built environment.</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Heads />
+      <Team />
+      <div className="cover-container section-3">
+        <div className="container text-left pt-5 pb-5 ">
+          <div className="row">
+            <div className="col">
+              <h3 className="text-white"> COLLEAGUE’S TESTIMONIALS</h3>
+              <hr className="bg-white border-4 border-top border-white"></hr>
+              For April:
+              <p className="col-sm-6">
+                Over the years, aside from April’s expertise in leading the
+                project team that has been instrumental to the growth of the
+                firm. April overcame many challenges through her hard work,
+                dedication and incredible work ethics. She is teachable,
+                coachable, resourceful and adapts well on the ever changing
+                demand of every project. April is also an effective
+                communicator. This skill set coupled with her insatiable
+                appetite for knowledge make her a brilliant and well-rounded
+                professional.
+              </p>
+              <p>
+                Arch. Teofilo “Boyet” Santiago, <br></br>
+                Jr. Senior Partner (Retired) <br></br>
+                GF & Partners Architects
+              </p>
+              <p>
+                For Chino: <br></br>
+                Request from Sir Arlie
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
